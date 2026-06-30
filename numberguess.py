4090370bbeba5c79a558e8a92ed7990ca2bbf4be
King <adarshnayak432@gmail.com>
2026-06-30 08:04:29 +0000

    Added Try again feature

--- numberguess.py
+++ numberguess.py
@@ -1,23 +1,38 @@
-import random
 
-magicnumber = random.randint(1,100)
 
-userinpt = int(input("Guess the magic number: "))
-tries = 0
+def playagain():
+    import random
 
-while userinpt!=magicnumber:
-    if userinpt>magicnumber:
-        print(f"The Magic number is less than {userinpt}")
-    else:
-        print(f"The Magic number is greater than {userinpt}")
+    magicnumber = random.randint(1,100)
+
+    userinpt = int(input("Guess the magic number: "))
+    tries = 0
+
+    while userinpt!=magicnumber:
+        if userinpt>magicnumber:
+            print(f"The Magic number is less than {userinpt}")
+        else:
+            print(f"The Magic number is greater than {userinpt}")
         
         
-    tries +=1
+        tries +=1
     
     
-    userinpt=int(input("Guess again: "))
+        userinpt=int(input("Guess again: "))
     
     
     
-print(f"""Hurray! You finally got it after {tries} tries.
+    print(f"""Hurray! You finally got it after {tries} tries.
 The Magic number is {magicnumber}""")
+    
+    x = input("If you want to play again then enter 'a', if you want to exit then enter anything else : ")
+    if x =="a":
+        playagain()
+    else:
+        return
+
+    
+    
+
+playagain()
+    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,24 @@
+    import random
+
+#     magicnumber = random.randint(1,100)
+
+#     userinpt = int(input("Guess the magic number: "))
+#     tries = 0
+
+#     while userinpt!=magicnumber:
+#         if userinpt>magicnumber:
+#             print(f"The Magic number is less than {userinpt}")
+#         else:
+#             print(f"The Magic number is greater than {userinpt}")
+        
+        
+#         tries +=1
+    
+    
+#         userinpt=int(input("Guess again: "))
+    
+    
+    
+#     print(f"""Hurray! You finally got it after {tries} tries.
+# The Magic number is {magicnumber}""")
+    
